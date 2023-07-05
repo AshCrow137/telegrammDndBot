c60989ea7bd33e106ddb7fc87406e639554aeef1
AshCrow137 <56866153+AshCrow137@users.noreply.github.com>
2023-07-05 11:54:08 +0500

    test commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+import random 
+
+bot= telebot.TeleBot('6358163919:AAHO1lf5BJLxQtxLM7mKKwO5ONh_5qMXiI8')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    id = message.chat.id
+    name = message.from_user.first_name
+    if not name:
+        name = message.from_user.username
+        if not name:
+            bot.send_message(id,f'Привет, кто бы ты ни был!')
+            return
+    bot.send_message(id,f'Привет, {name}')
+
+
+
+
+print('bot is on line')
+bot.infinity_polling(1)
